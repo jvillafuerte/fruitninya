e9dbc0ca7955b86d7d27490dbe9f4bafefa827d0
tonny <anthonny.gonzales@gmail.com>
2013-12-11 23:47:38 -0500

    textos

--- texto.h
+++ texto.h
@@ -1,4 +1,5 @@
 #include "MouseHandler.h"
+#include <time.h> 
 
 class Texto
 {
@@ -8,16 +9,33 @@ public:
     char buff[30];     //almncenamiento
     int nivel, frutas;  
     bool estado;       //para ver si imprime el texto de termino juego
-    
+    int tiempo;
+    time_t  tiempo_i;
+    struct tm * now ;
+    time_t tiempo_now;
+    bool tiempo_terminado;
+    int puntaje;
     Texto(){  // inicilizador
         font=(ptrdiff_t)GLUT_BITMAP_HELVETICA_18;
         font2=(ptrdiff_t)GLUT_BITMAP_HELVETICA_12;
         nivel = 1;
         frutas = 0;
         estado = false;
+        tiempo = 90;
+        tiempo_i = time(0);
+        now = localtime( & tiempo_i );
+        tiempo_terminado = false;
+        puntaje=0;
+    }
+    void actualizar_tiempo(){
+        time(&tiempo_now);
+        if(difftime(tiempo_now,tiempo_i)>=1)
+            tiempo--;
+        time(&tiempo_i);
     }
 
-    void set(){
+    void set_puntaje(int puntaje_){
+        puntaje=puntaje_;
     }
 
     char * to_char(int nivel)    //convertir de entero a string
@@ -25,6 +43,19 @@ public:
         sprintf (buff, "%d", nivel);
         return buff;
     }
+
+    char * time_to_char(int tiempo)    //convertir de entero a string
+    {
+        int min,sec;
+        min = tiempo/60;
+        sec = tiempo%60;
+        if(sec < 10) 
+            sprintf (buff, "%d:0%d", min, sec);
+        else
+            sprintf (buff, "%d:%d", min, sec);
+        return buff;
+    }
+
 
     void resetPerspectiveProjection() {   //reinicia la prespectiva de la preyeccion
         glMatrixMode(GL_PROJECTION);
@@ -43,14 +74,28 @@ public:
     void print(){
         glColor3f(1.0f, 1.0f, 1.0f);
         renderBitmapString(350,580,(void *)font,"Fruit Ninja");
-        renderBitmapString(650,550, (void*)font2, "Frutas Cortadas: ");   
-        renderBitmapString(650,580, (void*)font2, "Nivel: ");
-        renderBitmapString(690,580, (void*)font2, to_char(nivel));
-        renderBitmapString(750,550, (void*)font2, to_char(frutas));
+        renderBitmapString(10,560, (void*)font2, "Frutas Cortadas: "); 
+        renderBitmapString(10,540, (void*)font2, "Puntaje: ");   
+        renderBitmapString(10,580, (void*)font2, "Nivel: ");
+        renderBitmapString(650,580, (void*)font2, "Tiempo: ");
+        renderBitmapString(50,580, (void*)font2, to_char(nivel));
+        renderBitmapString(110,560, (void*)font2, to_char(frutas));
+        renderBitmapString(80,540, (void*)font2, to_char(puntaje));
+        if(!tiempo_terminado){
+            renderBitmapString(700,580, (void*)font2, time_to_char(tiempo));
+        }
+        else
+            renderBitmapString(700,580, (void*)font2, time_to_char(0));
         if(estado){
             glColor3f(1.0f, 0.0f, 0.0f);
             renderBitmapString(350,350,(void *)font,"Perdiste!");
         }
+        if(tiempo_terminado){
+            glColor3f(1.0f, 0.0f, 0.0f);
+            renderBitmapString(350,350,(void *)font,"Termino!");
+        }
+
+
     }
 
     void set_nivel(int n){
